62aae9184271b2120fb766805254dbfe2dcbf0b3
Dale Potter <dalepotter103@gmail.com>
2017-12-05 22:36:43 +0000

    Day 2 script, a coding counter to keep track of hours spent coding. Code prompts for time spent and comments about what was achieved. First time using datetime module.

---
+++ 002/CodeCounter.py
@@ -0,0 +1,31 @@
+#CodeCounter
+
+import datetime as dt
+
+def date():
+	date = dt.datetime.now()
+	date = str(date)
+	date = date.split('.')[0]
+	return date
+	
+def time():
+	print('How much time have you spent coding today, broke down to hours and minutes?')
+	hours = raw_input('How many hours spent coding:  ')
+	minutes = raw_input('How many minutes spent coding:  ')
+	return hours, minutes
+	
+def comments():
+	comment = raw_input('What were you working on? (Eg. project, course, ...)')
+	learn = raw_input('What new module or coding did you learn about?  ')
+	return comment, learn
+
+
+hour, min = time()
+comment, learnt = comments()
+day = date()
+	
+	
+
+storage = open('CodeCounterStorage.txt','a+')
+storage.write( day + ' ' +  hour + ' ' + min + ' ' + comment + ' ' + learnt + '\n')
+storage.close()
---
+++ 002/README.txt
@@ -0,0 +1,14 @@
+# CodeCounter
+
+This script will store hours of coding each day. Will be stored in a text file, in a formatt readable with pandas.
+
+input
+	prompt user for hours and mins
+	possibly also date
+	Small report of what was done
+	
+input then wrote to a file, saved then closed
+
+Useful links:
+	For adding to pandas later on
+	https://stackoverflow.com/questions/21546739/load-data-from-txt-with-pandas
