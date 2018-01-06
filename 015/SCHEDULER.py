d493579c6940b04fd2ebf9b369d1b5dec7078f23
Dale Potter <dalepotter103@gmail.com>
2018-01-06 21:08:24 +0000

    Comments added.

--- 015/SCHEDULER.py
+++ 015/SCHEDULER.py
@@ -1,12 +1,14 @@
+#Two modules used to scheduler execution of app
 from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
 	
-
-def some_job():
+#Fucntion to run Checkup.py, must also call 'python' for it to run
+def Checkup():
     subprocess.call(['python', 'Checkup.py'])
 
+#Create a block to schedule and add the job created earlier
 scheduler = BlockingScheduler()
-scheduler.add_job(some_job, 'interval', hours = 1)
+scheduler.add_job(Checkup, 'interval', hours = 1)
 scheduler.start()
 
 
